65990be0873d5127f8313fd78c50b29006a2c116
xaugat <sauugat@gmail.com>
2020-03-02 13:43:31 +0545

    updating home page ui

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,10 +10,9 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:alumniapp/logout.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Homepage extends StatelessWidget {
-
-
   String accessToken;
   Homepage(this.accessToken);
   @override
@@ -34,7 +33,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   TextEditingController searcheventController = TextEditingController();
   CarouselSlider carouselSlider;
   int _current = 0;
@@ -57,8 +55,10 @@ class _HomePageState extends State<HomePage> {
   _HomePageState(this.accessToken);
 
   Future<String> getJsonData() async {
-    var response = await http
-        .get(Uri.encodeFull(url), headers: {"Accept": "application/json", "search" : searcheventController.text});
+    var response = await http.get(Uri.encodeFull(url), headers: {
+      "Accept": "application/json",
+      "search": searcheventController.text
+    });
     print(response.body);
     var data = jsonDecode(response.body);
 
@@ -68,8 +68,6 @@ class _HomePageState extends State<HomePage> {
     print(userdata);
     return "Success";
   }
-
-
 
   List<T> map<T>(List list, Function handler) {
     List<T> result = [];
@@ -173,11 +171,10 @@ class _HomePageState extends State<HomePage> {
           ),
         ]),
       ),
-      body: 
-              Column(
-          children: <Widget>[
-            SizedBox(height: 10),
-             Container(
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 10),
+          Container(
             margin:
                 EdgeInsets.only(left: 20.0, right: 20.0, bottom: 8.0, top: 8.0),
             decoration: BoxDecoration(
@@ -200,11 +197,8 @@ class _HomePageState extends State<HomePage> {
                     hintText: 'Search events....',
                     contentPadding: EdgeInsets.only(left: 50.0),
                     border: InputBorder.none,
-                     
                   ),
-                  
                 ),
-               
                 Padding(
                   padding: const EdgeInsets.only(left: 320),
                   child: IconButton(
@@ -217,162 +211,192 @@ class _HomePageState extends State<HomePage> {
                       getJsonData();
                     },
                   ),
-                  
                 ),
               ],
             ),
           ),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 10,
-                  ),
-                
-                  carouselSlider = CarouselSlider(
-                    height: 350.0,
-                    initialPage: 0,
-                    enlargeCenterPage: true,
-                    autoPlay: true,
-                    reverse: false,
-                    enableInfiniteScroll: true,
-                    autoPlayInterval: Duration(seconds: 2),
-                    autoPlayAnimationDuration: Duration(milliseconds: 2000),
-                    pauseAutoPlayOnTouch: Duration(seconds: 10),
-                    scrollDirection: Axis.horizontal,
-                    onPageChanged: (index) {
-                      setState(() {
-                        _current = index;
-                      });
-                    },
-                    items: imgList.map((imgUrl) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 10.0),
-                            decoration: BoxDecoration(
-                              color: Colors.green,
-                            ),
-                            child: Image.asset(
-                              imgUrl,
-                              fit: BoxFit.fill,
-                            ),
-                          );
-                        },
-                      );
-                    }).toList(),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: map<Widget>(imgList, (index, image) {
-                      return Container(
-                        width: 10.0,
-                        height: 10.0,
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color:
-                              _current == index ? Colors.redAccent : Colors.green,
-                        ),
-                      );
-                    }),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text('We Celebrate Together!',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: userdata == null ? 0 : userdata.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    child: new Center(
-                      child: new Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          Card(
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 2, right: 4),
-                              child: ListTile(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => Eventdetail(
-                                          userdata[index]['event_name'],
-                                          userdata[index]['event_date'],
-                                          userdata[index]['event_time'],
-                                          userdata[index]['event_venue'],
-                                        ),
-                                      ));
-                                },
-                                leading: Icon(
-                                  Icons.event_available,
-                                  size: 50,
-                                  color: Colors.blue[900],
+          Expanded(
+            child: ListView.builder(
+              itemCount: userdata == null ? 0 : userdata.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  color: Colors.black,
+                  child: new Center(
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Card(
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 2, right: 4),
+                            child: ListTile(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Eventdetail(
+                                        userdata[index]['event_name'],
+                                        userdata[index]['event_date'],
+                                        userdata[index]['event_time'],
+                                        userdata[index]['event_venue'],
+                                      ),
+                                    ));
+                              },
+                              leading: Icon(
+                                Icons.event_available,
+                                size: 50,
+                                color: Colors.blue[900],
+                              ),
+                              title: Text(
+                                userdata[index]['event_name'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                  fontSize: 18,
                                 ),
-                                title: Text(
-                                  userdata[index]['event_name'],
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                    fontSize: 18,
+                              ),
+                              subtitle: Row(
+                                children: <Widget>[
+                                  Text(
+                                    userdata[index]['event_venue'],
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 1),
                                   ),
-                                ),
-                                subtitle: Row(
-                                  children: <Widget>[
-                                    Text(
-                                      userdata[index]['event_venue'],
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 1),
-                                    ),
-                                    SizedBox(
-                                      width: 5,
-                                    ),
-                                    Text(
-                                      userdata[index]['event_time'],
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 1),
-                                    ),
-                                  ],
-                                ),
-                                trailing: Column(
-                                  children: <Widget>[
-                                    Icon(
-                                      Icons.date_range,
-                                      color: Colors.black,
-                                    ),
-                                    SizedBox(height: 5),
-                                    Text(userdata[index]['event_date']),
-                                  ],
-                                ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    userdata[index]['event_time'],
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 1),
+                                  ),
+                                ],
+                              ),
+                              trailing: Column(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.date_range,
+                                    color: Colors.black,
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(userdata[index]['event_date']),
+                                ],
                               ),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
-        
-      
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 50),
+            child: Text(
+              'We welcome you !',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                SizedBox(width: 10),
+                SvgPicture.asset('images/games.svg', height: 50, width: 50,),
+                SizedBox(width: 30),
+                SvgPicture.asset('images/award.svg', height: 50, width: 50,),
+                 
+                SizedBox(width: 30),
+                
+                       
+               SvgPicture.asset('images/music.svg', height: 50, width: 50,),
+                   
+                SizedBox(width: 30),
+                SvgPicture.asset('images/giftsvg.svg', height: 50, width: 50,),
+               
+                    SizedBox(width:30),
+                    SvgPicture.asset('images/photo.svg', height: 40, width: 40,),
+              ],
+            ),
+          ),
+          Container(
+            width: 380,
+            color: Colors.black,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 10,
+                ),
+                carouselSlider = CarouselSlider(
+                  height: 350.0,
+                  initialPage: 0,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  reverse: false,
+                  enableInfiniteScroll: true,
+                  autoPlayInterval: Duration(seconds: 2),
+                  autoPlayAnimationDuration: Duration(milliseconds: 2000),
+                  pauseAutoPlayOnTouch: Duration(seconds: 10),
+                  scrollDirection: Axis.horizontal,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _current = index;
+                    });
+                  },
+                  items: imgList.map((imgUrl) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 10.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.green,
+                          ),
+                          child: Image.asset(
+                            imgUrl,
+                            fit: BoxFit.fill,
+                          ),
+                        );
+                      },
+                    );
+                  }).toList(),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: map<Widget>(imgList, (index, image) {
+                    return Container(
+                      width: 10.0,
+                      height: 10.0,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: _current == index ? Colors.blue : Colors.white,
+                      ),
+                    );
+                  }),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
